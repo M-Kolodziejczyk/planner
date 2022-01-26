a20873154692f03215cfa7b853094db2e4934354
MK <kolodziejczykmichal269@gmail.com>
2022-01-26 21:29:32 +0100

    base

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,10 +1,37 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+// import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { logger } from './common/middleware/logger.middleware';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
-  imports: [],
+  imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+// export class AppModule implements NestModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer
+//       .apply(LoggerMiddleware)
+//       .forRoutes({ path: 'cats', method: RequestMethod.GET });
+//   }
+// }
+
+// export class AppModule implements NestModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer.apply(LoggerMiddleware).forRoutes(CatsController);
+//   }
+// }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(logger).forRoutes(CatsController);
+  }
+}
