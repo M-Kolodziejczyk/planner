125fc242e1c333b466f9312794b93280e4431ea4
MK <kolodziejczykmichal269@gmail.com>
2022-02-09 22:56:40 +0100

    install depndencies

---
+++ packages/frontend/src/Pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AboutPage() {
+  return <div>About Pageee</div>;
+}
+
+export default AboutPage;
---
+++ packages/frontend/src/Root.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
+
+function Root() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default Root;
