addabc6016faeeb430b346f0a52a3ab77dc9a331
MK <kolodziejczykmichal269@gmail.com>
2022-02-14 22:26:37 +0100

    create notfound page

--- packages/frontend/src/Root.tsx
+++ packages/frontend/src/Root.tsx
@@ -1,16 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "./Pages/HomePage";
 import AboutPage from "./Pages/AboutPage";
+import NotFound from "./Pages/NotFound";
 
 function Root() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
